b03dcc481d0a43c284407bab089de7dbb8dcfd8b
Omer Yassir <omerym10@gmail.com>
2024-08-07 15:05:08 +0300

    sadg

--- countriesviewer/src/App.tsx
+++ countriesviewer/src/App.tsx
@@ -2,16 +2,21 @@ import './css/App.css'
 import Navbar from './Navbar';
 import HomePage from './HomePage';
 import useTheme, { getOppisite } from './useTheme';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
     const [theme, toggleTheme] = useTheme();
     return (
         <>
-            <header>
-                <Navbar>
-                    <button className="theme-button" onClick={toggleTheme}>{getOppisite(theme)} theme</button>
-                </Navbar>
-            </header>
-            <HomePage />
+            <BrowserRouter basename="/CountriesViewer">
+                <header>
+                    <Navbar>
+                        <button className="theme-button" onClick={toggleTheme}>{getOppisite(theme)} theme</button>
+                    </Navbar>
+                </header>
+                <Routes>
+                    <Route path="/" index element={<HomePage />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
